e215fbbfbc7396db64358093e8ba44a99d72c284
keyhong <wnhong96@gmail.com>
2025-08-04 00:07:27 +0900

    add ubuntu packages

--- 2025.01.3-b8/packages/bin/download-ubuntu-packages.sh
+++ 2025.01.3-b8/packages/bin/download-ubuntu-packages.sh
@@ -8,17 +8,33 @@ PKG_ROOT="${JUPYTER_SERVER_ROOT}/packages/ubuntu_packages"
 
 # 패키지와 디렉토리 매핑
 declare -A packages=(
-    ["powerline"]="powerline"
+    # https://github.com/vim/vim/blob/master/README.md
     ["vim"]="vim"
+    # https://github.com/scop/bash-completion/blob/main/README.md
     ["bash-completion"]="bash-completion"
+    # https://nbconvert.readthedocs.io/en/latest/install.html#installing-pandoc
     ["pandoc"]="pandoc"
-    ["texlive-xetex"]="texlive-xetex"
+    # https://nbconvert.readthedocs.io/en/latest/install.html#installing-tex
+    ["texlive-xetex"]="texlive-xetex" 
     ["texlive-fonts-recommended"]="texlive-fonts-recommended"
     ["texlive-plain-generic"]="texlive-plain-generic"
+    # https://zsh.sourceforge.io/
     ["zsh"]="zsh"
+    # https://www.unixodbc.org/
     ["unixodbc"]="unixodbc"
     ["unixodbc-dev"]="unixodbc-dev"
+    # https://github.com/powerline/powerline/blob/develop/README.rst
+    ["powerline"]="powerline"
+    # https://github.com/powerline/fonts/blob/master/README.rst
     ["fonts-powerline"]="fonts-powerline"
+    # https://sourceforge.net/p/net-tools/code/ci/master/tree/README
+    ["net-tools"]="net-tools"
+    # https://github.com/iputils/iputils/blob/master/README.md
+    ["iputils-ping"]="iputils-ping"
+    # https://gitlab.isc.org/isc-projects/bind9/-/blob/main/README.md
+    ["dnsutils"]="dnsutils"
+    # https://dos2unix.sourceforge.io/
+    ["dos2unix"]="dos2unix"
 )
 
 # 일반 패키지 다운로드 및 이동
